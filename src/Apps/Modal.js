12ab1451b59251acf4e0ec43850749d34c9a45d5
dcorso21 <david.l.corson@gmail.com>
2020-10-15 12:31:34 -0400

    info card styled

--- src/Apps/Modal.js
+++ src/Apps/Modal.js
@@ -7,13 +7,35 @@ const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
 export default function Modal({ clickedMovie, isOpen }) {
     if (!isOpen) return null;
+    const MODAL_STYLES = {
+        background: `linear-gradient(to bottom, rgba(0, 0, 0, .5), #000), url(${
+            IMGPATH + clickedMovie.backdrop_path
+        })`,
+        backgroundSize: "cover",
+    };
     console.log(clickedMovie);
     return ReactDom.createPortal(
         <>
             <div className="Overlay"></div>
-            <div className="Modal">
-                <div className="mtitle">{clickedMovie.title}</div>
-                <img src={IMGPATH + clickedMovie.backdrop_path} alt="" />
+            <div className="Modal" style={MODAL_STYLES}>
+                <div className="modalContent">
+                <div className="card-title">{clickedMovie.title}</div>
+                <div className="infoCard">
+                    <div>
+                        <h3>Info</h3>
+                        <div className="releaseDate">
+                            Release Date: {clickedMovie.release_date}
+                        </div>
+                        <div className="voteAverage">
+                            Average Vote: {clickedMovie.vote_average}
+                        </div>
+                    </div>
+                    <div className="overview">
+                        <h3>Overview</h3>
+                        <p>{clickedMovie.overview}</p>
+                    </div>
+                </div>
+                </div>
             </div>
         </>,
         document.getElementById("Modal-portal")
