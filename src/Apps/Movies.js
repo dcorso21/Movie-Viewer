6e64421d3c51e1e42ab5aef5c751c631ceb344d9
David Corson <david.l.corson@gmail.com>
2021-06-10 21:17:02 -0400

    added support for no image

--- src/Apps/Movies.js
+++ src/Apps/Movies.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+
 import "./Movies.css";
 import Modal from "./Modal.js";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
@@ -20,7 +21,11 @@ export default function Movies({ searchResults }) {
                 onClick={() => handleClickMovie(movieInfo)}
             >
                 <img
-                    src={IMGPATH + movieInfo.poster_path}
+                    src={
+                        movieInfo.poster_path
+                            ? IMGPATH + movieInfo.poster_path
+                            : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.rT9U1F6v5-uR5jr6IxdvpgAAAA%26pid%3DApi&f=1"
+                    }
                     alt={movieInfo.title}
                 />
                 <div className="info">
